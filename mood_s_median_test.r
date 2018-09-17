b321bd967323ba959cacd35a1a3a99d4a4f248a9
michbur <michalburdukiewicz@gmail.com>
2018-09-17 14:07:06 +0200

    improved mood test

--- mood_s_median_test.r
+++ mood_s_median_test.r
@@ -2,5 +2,9 @@ median.test <- function(x, y){
     z <- c(x, y)
     g <- rep(1:2, c(length(x), length(y)))
     m <- median(z)
-    fisher.test(z < m, g)
+    if(length(unique(z < m)) == 1) {
+      list(p.value = 1)
+    } else {
+      fisher.test(z < m, g)
+    }
 }
